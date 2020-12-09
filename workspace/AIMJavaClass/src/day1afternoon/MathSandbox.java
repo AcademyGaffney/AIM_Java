26d6fce7bf56221814d9b2827255b067147c51e7
AcademyGaffney <michaelkgaffney@gmail.com>
2020-12-09 09:25:09 -0600

    3

--- workspace/AIMJavaClass/src/day1afternoon/MathSandbox.java
+++ workspace/AIMJavaClass/src/day1afternoon/MathSandbox.java
@@ -5,6 +5,12 @@ public class MathSandbox {
 	public static void main(String[] args) {
 
 		System.out.println((byte)128.999993);
+		
+		int a = 12;
+		int b = 7; 
+		int c = 8;
+		
+		
 
 	}
 
--- workspace/AIMJavaClass/src/day1afternoon/StringExercises.java
+++ workspace/AIMJavaClass/src/day1afternoon/StringExercises.java
@@ -29,5 +29,31 @@ public class StringExercises {
 		String [] words = s.split(" ");
 		return words[2];
 	}
+	
+	public static String exercise2(String s) {//"every good boy deserves favor"
+		String s1 = s.substring(0,1).toUpperCase();//"E"
+		int sp1 = s.indexOf(" ");//5
+		String s2 = s.substring(1, sp1+1).toLowerCase();//(1, 6): "very "
+		String s3 = s.substring(sp1+1, sp1+2).toUpperCase();
+		int sp2 = s.indexOf(" ", sp1+1);
+		String s4 = s.substring(sp1+2, sp2+1).toLowerCase();
+		String s5 = s.substring(sp2+1, sp2+2).toUpperCase();
+		String s6 = s.substring(sp2+2).toLowerCase();
+		
+		return s1+s2+s3+s4+s5+s6;
+		
+	}
+	
+	public static String exercise2a(String s) {//"every good boy deserves favor"
+		int sp1 = s.indexOf(" ");//5
+		int sp2 = s.indexOf(" ", sp1+1);
+		int sp3 = s.indexOf(" ", sp2+1);
+		String s4 = s.substring(0, sp3+1).toUpperCase();
+		String s5 = s.substring(sp3+1);
+
+		
+		return s4+s5;
+		
+	}
 
 }
